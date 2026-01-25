3cf65de3980bfbf54ea94864d3785e8b0a8306c9
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-01-24 19:13:52 -0800

    edtdtfhcd

--- projects/Pop-ups/Pong/Pong.js
+++ projects/Pop-ups/Pong/Pong.js
@@ -1,2 +1,15 @@
 const startButton = document.getElementById("startButton");
 
+const startGame = () => {
+  const playerWidth = 450;
+  const playerHeight = 150;
+
+  let p1x = (screen.width/2) - (playerWidth/2);
+  let p2x = p1x;
+
+  const p1Features = `width=${playerWidth},height=${playerHeight},left=${p1x},top=${0},popup=yes`;
+
+  const p1 = window.open('', 'Player1', p1Features);
+};
+
+startButton.addEventListener('click', startGame);
