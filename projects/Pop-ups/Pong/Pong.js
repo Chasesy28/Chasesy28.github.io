2e4109840914bf79a98b7d46844597e067b82877
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-01-24 22:37:48 -0800

    ojmhdrgrxdx

--- projects/Pop-ups/Pong/Pong.js
+++ projects/Pop-ups/Pong/Pong.js
@@ -1,21 +1,23 @@
 const startButton = document.getElementById("startButton");
 
+let p1;
+let p2;
+const playerWidth = 450;
+const playerHeight = 50;
+let p1x = (screen.width/2) - (playerWidth/2);
+let p2x = p1x;
+let pSpeed = 2;
+const p1Features = `width=${playerWidth},height=${playerHeight},left=${p1x},top=${0},popup=yes`;
+
 const startGame = () => {
-  const playerWidth = 450;
-  const playerHeight = 50;
-
-  let p1x = (screen.width/2) - (playerWidth/2);
-  let p2x = p1x;
-
-  let pSpeed = 2;
-
-  const p1Features = `width=${playerWidth},height=${playerHeight},left=${p1x},top=${0},popup=yes`;
-
-  const p1 = window.open('', 'Player1', p1Features);
+  p1 = window.open('', 'Player1', p1Features);
   p1.moveTo(p1x,0);
   p1.resizeTo(playerWidth,playerHeight);
+};
 
-  document.addEventListener('keydown', (e) => {
+startButton.addEventListener('click', startGame);
+
+document.addEventListener('keydown', (e) => {
   switch(e.key){
     case "ArrowRight":
       p1.moveBy(pSpeed, 0);
@@ -23,8 +25,4 @@ const startGame = () => {
     case "ArrowLeft":
       p1.moveBy(-pSpeed, 0);
       break;
-  }
 });
-};
-
-startButton.addEventListener('click', startGame);
