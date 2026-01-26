aa9f880fd8e42bb93f74290ea8872446fd502b53
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-01-26 13:53:48 -0800

    oubouwbeode

--- projects/Pop-ups/Pong/Pong.js
+++ projects/Pop-ups/Pong/Pong.js
@@ -11,49 +11,11 @@ const p1Features = `width=${playerWidth},height=${playerHeight},left=${p1x},top=
 const p2Features = `width=${playerWidth},height=${playerHeight},left=${p2x},top=${screen.availHeight},popup=yes`;
 
 const playerMovement = (e) => {
-  switch(e.key){
-    case "ArrowRight":
-      p1.focus();
-      p2.focus();
-      p1.moveBy(pSpeed, 0);
-    case "ArrowLeft":
-      p1.focus();
-      p2.focus();
-      p1.moveBy(-pSpeed, 0);
-    case "D":
-    case "d":
-      p1.focus();
-      p2.focus();
-      p2.moveBy(pSpeed, 0);
-    case "A":
-    case "a":
-      p1.focus();
-      p2.focus();
-      p2.moveBy(-pSpeed, 0);
-  }
-  p1x = p1.screenX;
-  p1.moveTo(p1x, 0);
-  p1.resizeTo(playerWidth,playerHeight);
-  p2x = p2.screenX;
-  p2.moveTo(p2x, screen.availHeight);
-  p2.resizeTo(playerWidth,playerHeight);
+
 }
 
 const startGame = () => {
-  p1x = (screen.width/2) - (playerWidth/2);
-  p2x = p1x;
-  p1 = window.open('', 'Player1', p1Features);
-  p1.moveTo(p1x,0);
-  p1.resizeTo(playerWidth,playerHeight);
-  p2 = window.open('', 'Player2', p2Features);
-  p2.moveTo(p2x,screen.availHeight);
-  p2.resizeTo(playerWidth,playerHeight);
-  document.removeEventListener('keydown', playerMovement);
-  document.addEventListener('keydown', playerMovement);
-  p1.removeEventListener('keydown', playerMovement);
-  p1.addEventListener('keydown', playerMovement);
-  p2.removeEventListener('keydown', playerMovement);
-  p2.addEventListener('keydown', playerMovement);
+  p1 = window.open('','',p1Features);
 };
 
 startButton.addEventListener('click', startGame);
