de87185899630715140bddadf4aba8cdf66450b0
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-01-26 14:06:23 -0800

    ovyhohvihy

--- projects/Pop-ups/Pong/Pong.js
+++ projects/Pop-ups/Pong/Pong.js
@@ -2,13 +2,13 @@ const startButton = document.getElementById("startButton");
 
 let p1;
 let p2;
-const playerWidth = 450;
-const playerHeight = 50;
+const playerWidth = 50;
+const playerHeight = 450;
 let pSpeed = 5;
-let p1x = (screen.width/2) - (playerWidth/2);
-let p2x = p1x;
-const p1Features = `width=${playerWidth},height=${playerHeight},left=${p1x},top=${0},popup=yes`;
-const p2Features = `width=${playerWidth},height=${playerHeight},left=${p2x},top=${screen.availHeight},popup=yes`;
+let p1y = (screen.width/2) - (playerWidth/2);
+let p2y = p1y;
+const p1Features = `width=${playerWidth},height=${playerHeight},left=${0},top=${p1y},popup=yes`;
+const p2Features = `width=${playerWidth},height=${playerHeight},left=${screen.availWidth},top=${p2y},popup=yes`;
 
 const playerMovement = (e) => {
 
@@ -16,6 +16,8 @@ const playerMovement = (e) => {
 
 const startGame = () => {
   p1 = window.open('','',p1Features);
+  p1.resizeTo(playerWidth,playerHeight);
+  p1.moveTo(0,p1y);
 };
 
 startButton.addEventListener('click', startGame);
