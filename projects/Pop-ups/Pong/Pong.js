da1ab7f8a31c1d797e2ed4aaccf7c249f3a14b3b
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-01-28 23:38:34 -0800

    sthrfg

--- projects/Pop-ups/Pong/Pong.js
+++ projects/Pop-ups/Pong/Pong.js
@@ -8,7 +8,7 @@ let pSpeed = 5;
 let p1y = screen.height / 2 - pHeight / 2;
 let p2y = p1y;
 const p1Features = `width=${pWidth},height=${pHeight},left=${0},top=${p1y},popup=yes`;
-const p2Features = `width=${pWidth},height=${pHeight},left=${screen.width - pWidth - 20},top=${p2y},popup=yes`;
+const p2Features = `width=${pWidth},height=${pHeight},left=${screen.width - pWidth},top=${p2y},popup=yes`;
 
 const controller = {
   W: { pressed: false, dir: "up", char: 1 },
@@ -28,13 +28,9 @@ function updatePlayerValues() {
         pSpeed = -pSpeed;
       }
       if (controller[key].char == 1) {
-        console.log(`p1y before: ${p1y}`);
         p1y += pSpeed;
-        console.log(`p1y after: ${p1y}`);
       } else {
-        console.log(`p2y before: ${p2y}`);
         p2y += pSpeed;
-        console.log(`p2y after: ${p2y}`);
       }
     });
   }
@@ -46,7 +42,7 @@ function updatePlayerValues() {
       }
     });
     p1.moveTo(0, p1y);
-    p2.moveTo(screen.width - pWidth - 20, p2y);
+    p2.moveTo(screen.width - pWidth, p2y);
     p1.resizeTo(pWidth, pHeight);
     p2.resizeTo(pWidth, pHeight);
     p1.focus();
