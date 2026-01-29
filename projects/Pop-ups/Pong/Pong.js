8abb9485f4116be69f74b266de60fa50c7db4553
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-01-28 21:32:52 -0800

    8b6dft9oymibnj

--- projects/Pop-ups/Pong/Pong.js
+++ projects/Pop-ups/Pong/Pong.js
@@ -5,103 +5,106 @@ let p2;
 let playerWidth = 150;
 let playerHeight = 400;
 let pSpeed = 5;
-let p1y = (screen.height / 2) - (playerHeight / 2);
+let p1y = screen.height / 2 - playerHeight / 2;
 let p2y = p1y;
 const p1Features = `width=${playerWidth},height=${playerHeight},left=${0},top=${p1y},popup=yes`;
 const p2Features = `width=${playerWidth},height=${playerHeight},left=${screen.availWidth},top=${p2y},popup=yes`;
 
-const resetPlayerValues = () => {
+const controller = {
+  ArrowUp: { pressed: false, dir: "up", char: 1 },
+  ArrowDown: { pressed: false, dir: "down", char: 1 },
+  W: { pressed: false, dir: "up", char: 2 },
+  w: { pressed: false, dir: "up", char: 2 },
+  S: { pressed: false, dir: "down", char: 2 },
+  s: { pressed: false, dir: "down", char: 2 },
+};
+
+function updatePlayerValues() {
   p1.resizeTo(playerWidth, playerHeight);
-  p1.moveTo(0, p1y);
   p2.resizeTo(playerWidth, playerHeight);
-  p2.moveTo(screen.availWidth, p2y);
+
+  function pMovementCalc() {
+    Object.keys(controller).forEach((key) => {
+      pSpeed = 5;
+      if (controller[key].dir == "down") {
+        pSpeed = -pSpeed;
+      }
+      if (controller[key].char == 1) {
+        p1y += pSpeed;
+      } else {
+        p2y += pSpeed;
+      }
+    });
+  }
+
+  function pMove() {
+    Object.keys(controller).forEach((key) => {
+      if (controller[key].pressed) {
+        pMovementCalc;
+      }
+    });
+    p1.moveTo(0, p1y);
+    p2.moveTo(0, p2y);
+  }
+
+  pMove();
+}
+
+const startGame = () => {
+  if (p1) {
+    p1.close();
+  }
+  p1 = window.open("", "Player 1", p1Features);
+  if (p2) {
+    p2.close();
+  }
+  p2 = window.open("", "Player 2", p2Features);
   if (p1 && p2) {
+    updatePlayerValues();
     const closeCheck = setInterval(function () {
       if (p1.closed) {
         p2.close();
         clearInterval(closeCheck);
-      }
-      else if (p2.closed) {
+      } else if (p2.closed) {
         p1.close();
         clearInterval(closeCheck);
       }
-    }, 500)
+    }, 500);
+  } else {
+    alert("Popup Windows are Blocked!");
   }
-};
-
-//Movement Code
-const controller = {
-  "ArrowUp":{pressed:false, dir:"up", char:1},
-  "ArrowDown":{pressed:false, dir:"down", char:1},
-  "W":{pressed:false, dir:"up", char:2}, "w":{pressed:false, dir:"up", char:2},
-  "S":{pressed:false, dir:"down", char:2}, "s":{pressed:false, dir:"down", char:2}
-};
-
-const playerMovement = () => {
-  console.log("playerMovement run");
-  Object.keys(controller).forEach(key => {
-    if(controller[key].pressed){
-      if(controller[key].dir == "down"){
-        pSpeed = -pSpeed;
-      }
-      if(controller[key].char == 1){
-          p1.moveBy(0,pSpeed);
-          p1y += pSpeed;
-        }
-      else{
-        p2.moveBy(0,pSpeed);
-        p2y += pSpeed;
-      }
-    }
-  });
-  pSpeed = 5;
-  resetPlayerValues();
-};
-
-const startGame = () => {
-  if (p1) { p1.close(); }
-  p1 = window.open('', 'Player 1', p1Features);
-  if (p2) { p2.close(); }
-  p2 = window.open('', 'Player 2', p2Features);
-  resetPlayerValues();
 
   document.addEventListener("keydown", (e) => {
-    if(controller[e.key]){
+    if (controller[e.key]) {
       controller[e.key].pressed = true;
       console.log(`${controller[e.key]} pressed down`);
     }
   });
   document.addEventListener("keyup", (e) => {
-    if(controller[e.key]){
+    if (controller[e.key]) {
       controller[e.key].pressed = false;
-      console.log(`${controller[e.key]} released`);
     }
   });
   p1.addEventListener("keydown", (e) => {
-    if(controller[e.key]){
+    if (controller[e.key]) {
       controller[e.key].pressed = true;
-      console.log(`${controller[e.key]} pressed down`);
     }
   });
   p1.addEventListener("keyup", (e) => {
-    if(controller[e.key]){
+    if (controller[e.key]) {
       controller[e.key].pressed = false;
-      console.log(`${controller[e.key]} released`);
     }
   });
   p2.addEventListener("keydown", (e) => {
-    if(controller[e.key]){
+    if (controller[e.key]) {
       controller[e.key].pressed = true;
-      console.log(`${controller[e.key]} pressed down`);
     }
   });
   p2.addEventListener("keyup", (e) => {
-    if(controller[e.key]){
+    if (controller[e.key]) {
       controller[e.key].pressed = false;
-      console.log(`${controller[e.key]} released`);
     }
   });
-  const moveLoop = setInterval(playerMovement, 500);
 };
 
-startButton.addEventListener('click', startGame);
+startButton.addEventListener("click", startGame);
