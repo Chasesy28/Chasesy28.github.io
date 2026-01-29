8b864ad79fad426e9ad8bf2b5f10186014569b1f
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-01-28 22:04:38 -0800

    weohngeordfl

--- projects/Pop-ups/Pong/Pong.js
+++ projects/Pop-ups/Pong/Pong.js
@@ -2,13 +2,13 @@ const startButton = document.getElementById("startButton");
 
 let p1;
 let p2;
-let playerWidth = 150;
-let playerHeight = 400;
+let pWidth = 150;
+let pHeight = 400;
 let pSpeed = 5;
-let p1y = screen.height / 2 - playerHeight / 2;
+let p1y = screen.height / 2 - pHeight / 2;
 let p2y = p1y;
-const p1Features = `width=${playerWidth},height=${playerHeight},left=${0},top=${p1y},popup=yes`;
-const p2Features = `width=${playerWidth},height=${playerHeight},left=${screen.availWidth},top=${p2y},popup=yes`;
+const p1Features = `width=${pWidth},height=${pHeight},left=${0},top=${p1y},popup=yes`;
+const p2Features = `width=${pWidth},height=${pHeight},left=${screen.availWidth-pWidth},top=${p2y},popup=yes`;
 
 const controller = {
   W: { pressed: false, dir: "up", char: 1 },
@@ -37,13 +37,13 @@ function updatePlayerValues() {
   function pMove() {
     Object.keys(controller).forEach((key) => {
       if (controller[key].pressed) {
-        pMovementCalc;
+        pMovementCalc();
       }
     });
     p1.moveTo(0, p1y);
-    p2.moveTo(screen.availWidth, p2y);
-    p1.resizeTo(playerWidth, playerHeight);
-    p2.resizeTo(playerWidth, playerHeight);
+    p2.moveTo(screen.availWidth-pWidth, p2y);
+    p1.resizeTo(pWidth, pHeight);
+    p2.resizeTo(pWidth, pHeight);
     p1.focus();
     p2.focus();
 
