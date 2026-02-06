2e2bdab1de0394526652c12c63aa270c07fa2b20
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-02-06 20:31:42 +0000

    eourhijn

--- projects/Pop-ups/Pong/Pong.js
+++ projects/Pop-ups/Pong/Pong.js
@@ -86,7 +86,7 @@ function updateValues() {
       ballSpeedY = -ballSpeedY;
     }
 
-    if (ballY + ballHeight >= p1y && ballY <= p1y + pHeight) {
+    if (ballX + ballWidth <= pWidth) {
       ballSpeedX = -ballSpeedX;
     }
     if (ballX + ballWidth >= screen.availWidth - pWidth && ballY + ballHeight >= p2y && ballY <= p2y + pHeight) {
