b7f62682093c2fe47928a1cbf8e2a6f224f33006
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-02-10 22:33:53 -0800

    Update collision guard constants in Pong.js to improve gameplay boundaries

--- projects/Pop-ups/Pong/Pong.js
+++ projects/Pop-ups/Pong/Pong.js
@@ -29,8 +29,8 @@ const edgeOffsets = {
   top: 0,
 };
 
-const POSITION_RIGHT_GUARD = 200;
-const COLLISION_RIGHT_INSET = 200;
+const POSITION_RIGHT_GUARD = 0;
+const COLLISION_RIGHT_INSET = 0;
 
 const ensureSize = (win, width, height) => {
   if (!win) {
