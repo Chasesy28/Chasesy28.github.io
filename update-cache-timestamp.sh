d729202b375553c676293959a824c283d59045a2
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-02-25 19:52:34 +0000

    a

--- update-cache-timestamp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Updates the CACHE_TIMESTAMP constant in sw.js to the current UTC timestamp.
-
-TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-
-sed -i "s|const CACHE_TIMESTAMP = \"[^\"]*\";|const CACHE_TIMESTAMP = \"$TIMESTAMP\";|" sw.js
-
-echo "CACHE_TIMESTAMP updated to $TIMESTAMP"
