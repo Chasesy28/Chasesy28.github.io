421a3a6c9462b898a33a4cfd18d3160317612795
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-02-25 20:21:00 +0000

    u

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // Automatic cache versioning with timestamp - update this when deploying new versions
 // This ensures users get fresh content when the site is updated
 const CACHE_VERSION = "7";
-const CACHE_TIMESTAMP = "2026-02-25T20:19:14Z"; // Update this timestamp when deploying
+const CACHE_TIMESTAMP = "2026-02-25T20:20:54Z"; // Update this timestamp when deploying
 const CACHE_NAME = `silly-site-cache-v${CACHE_VERSION}-${CACHE_TIMESTAMP}`;
 
 /**
