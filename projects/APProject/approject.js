63cf73ec5434de2a85f362438fc544e640611be4
Umechas <chase.bremer28@student.tamdistrict.org>
2026-03-14 16:43:39 -0700

    AP project setup

---
+++ projects/APProject/approject.js
@@ -0,0 +1,19 @@
+const gameArea = document.getElementById("gameArea");
+const ctx = gameArea.getContext("2d");
+
+gameArea.style.width = "100dvw";
+gameArea.style.height = "100dvh";
+gameArea.width = gameArea.offsetWidth;
+gameArea.height = gameArea.offsetHeight;
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+}
+
+document.addEventListener("click", toggleFullScreen);
