79b740af0136486573bcabcb704c1961849e15e8
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-03-30 16:16:22 +0000

    s

--- projects/WebGl-Test/WebGL-Library/webgl.js
+++ projects/WebGl-Test/WebGL-Library/webgl.js
@@ -1,18 +1,13 @@
-const canvas = document.getElementById("canvas");
-const gl = canvas.getContext("2d");
+const canvas = document.getElementById("webglCanvas");
+const gl = canvas.getContext("webgl");
 
-/*canvas.style.width = "100dvw";
+canvas.style.width = "100dvw";
 canvas.style.height = "100dvh";
 canvas.width = canvas.offsetWidth;
-canvas.height = canvas.offsetHeight;*/
+canvas.height = canvas.offsetHeight;
 
 if (!gl) {
   alert("WebGL not supported in this browser.");
 }
 
-//backgroundColor(gl, [0.0, 0.0, 0.0, 1.0]); // Set background color to black
-
-//gl.clearColor(0.0, 0.0, 0.0, 1.0); // Set background color to black
-//gl.clear(gl.COLOR_BUFFER_BIT); // Clear the color buffer with the specified clear color
-gl.fillStyle = "red";
-gl.fillRect(50, 50, 100, 100); // Draw a red square at (50, 50) with width and height of 100 pixels
+backgroundColor(gl, [0.0, 0.0, 0.0, 1.0]); // Set background color to black
