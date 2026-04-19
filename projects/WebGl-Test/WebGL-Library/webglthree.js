e6e09405fd82817e71f7af0e97e6c7b0a8a9ab4f
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-04-18 17:36:47 -0700

    s

--- projects/WebGl-Test/WebGL-Library/webglthree.js
+++ projects/WebGl-Test/WebGL-Library/webglthree.js
@@ -11,10 +11,13 @@ const boxDepth = 1;
 const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
 const color = 0xFFFFFF;
-const intensity = 3;
+const intensity = 1;
 const light = new THREE.DirectionalLight(color, intensity);
-light.position.set(-1, 2, 4);
+light.position.set(5, 5, 5);
 scene.add(light);
+
+const ambientLight = new THREE.AmbientLight(0x404040, 2); // soft white light
+scene.add(ambientLight);
 
 camera.position.z = 5;
 
@@ -29,16 +32,19 @@ function makeInstance(x, y, z, geometry, color) {
   return cube;
 }
 
+const loader = new THREE.TextureLoader();
+const texture = loader.load("/images/SuperMarioTitle.png");
+
 const cubes = [
-  makeInstance(0, 0, 0, geometry, 0x44aa88),
+  makeInstance(0, 0, 0, geometry, texture),
   makeInstance(-2, 0, 0, geometry, 0x8844aa),
   makeInstance(1, 0, 0, geometry, 0xaa8844),
 ];
 
-let cameraPosition = [0, 0, 5];
 let cameraRotation = [0, 0, 0];
 
-const speed = 0.1;
+const moveSpeed = 0.1;
+const rotationSpeed = 0.025;
 
 function animate(time) {
   time *= 0.001;  // convert time to seconds
@@ -49,75 +55,65 @@ function animate(time) {
   camera.aspect = canvas.clientWidth / canvas.clientHeight;
   camera.updateProjectionMatrix();
 
-
-  /*cubes.forEach((cube, ndx) => {
-    const speed = 1 + ndx * .1;
-    const rot = time * speed;
-    cube.rotation.x = rot;
-    cube.rotation.y = rot;
-  });*/
-
   cubes.forEach((cube) => {
-    cube.material = new THREE.MeshPhongMaterial({color: new THREE.Color(1, 0, 0)});
+    cube.material.map = texture;
   });
 
   if (controller.forward.pressed) {
     const forward = new THREE.Vector3();
     camera.getWorldDirection(forward);
     forward.y = 0;
-    camera.position.addScaledVector(forward, speed);
+    camera.position.addScaledVector(forward, moveSpeed);
   }
   if (controller.backward.pressed) {
     const backward = new THREE.Vector3();
     camera.getWorldDirection(backward);
     backward.y = 0;
-    camera.position.addScaledVector(backward, -speed);
+    camera.position.addScaledVector(backward, -moveSpeed);
   }
   if (controller.left.pressed) {
     const left = new THREE.Vector3();
     camera.getWorldDirection(left);
     left.y = 0;
     left.cross(camera.up);
-    camera.position.addScaledVector(left, -speed);
+    camera.position.addScaledVector(left, -moveSpeed);
   }
   if (controller.right.pressed) {
     const right = new THREE.Vector3();
     camera.getWorldDirection(right);
     right.y = 0;
     right.cross(camera.up);
-    camera.position.addScaledVector(right, speed);
+    camera.position.addScaledVector(right, moveSpeed);
   }
   if (controller.up.pressed) {
     const up = new THREE.Vector3(0, 1, 0);
-    camera.position.addScaledVector(up, speed);
+    camera.position.addScaledVector(up, moveSpeed);
   }
   if (controller.down.pressed) {
     const down = new THREE.Vector3(0, 1, 0);
-    camera.position.addScaledVector(down, -speed);
+    camera.position.addScaledVector(down, -moveSpeed);
   }
 
   if (controller.lookUp.pressed) {
-    cameraRotation[0] += 0.01;
+    camera.rotation.x += rotationSpeed;
   }
   if (controller.lookDown.pressed) {
-    cameraRotation[0] -= 0.01;
+    camera.rotation.x -= rotationSpeed;
   }
   if (controller.lookLeft.pressed) {
-    cameraRotation[1] += 0.01;
+    cameraRotation[1] += rotationSpeed;
   }
   if (controller.lookRight.pressed) {
-    cameraRotation[1] -= 0.01;
+    cameraRotation[1] -= rotationSpeed;
   }
   if (controller.resetView.pressed) {
     cameraRotation = [0, 0, 0];
   }
 
-  camera.rotation.x = cameraRotation[0];
+  //camera.rotation.x = cameraRotation[0];
   camera.rotation.y = cameraRotation[1];
 
-  light.position.x = camera.position.x;
-  light.position.y = camera.position.y;
-  light.position.z = camera.position.z;
+  light.position.copy(camera.position);
 
   renderer.render(scene, camera);
 
