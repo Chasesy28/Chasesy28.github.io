30082b57088bde495ad24df0c3f656a8a3863e25
benjaminzimmet28-hub <benjamin.zimmet28@student.tamdistrict.org>
2026-06-05 18:49:03 +0000

    s

--- projects/Games/Multiplayer/Test/test.js
+++ projects/Games/Multiplayer/Test/test.js
@@ -4,7 +4,7 @@ import {
   loginWithGoogle,
   logout,
   supabase,
-} from './panel-supabase.js'
+} from 'silly-site.me/projects/Admin/panel-supabase.js'
 
 const gameArea = document.getElementById("gameArea");
 const ctx = gameArea.getContext("2d");
